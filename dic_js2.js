f5d27a41c8ceff1d16cb2bf11e138c6a04de0b0b
yshrimp <eogksla123@gmail.com>
2021-05-18 22:46:17 +0900

    word_relay

--- dic_js2.js
+++ dic_js2.js
@@ -41,3 +41,40 @@ debug.log('Hello, Method')
 모든 가능성을 고려해야 한다.
 예시는 절차를 검증하는데 사용한다.
 */
+
+/* prompt alert confrim
+prompt 사용자에게 정보를 입력받는 함수
+alert 사용자에게 경고창을 띄우는
+confirm 사용자에게 확인(예/아니요)을 받는
+*/
+
+// HTML 태그 선택하기(querySelector)
+document.querySelector('선택자')
+// id선택자 = #id class선택자 = .class
+document.querySelectorAll() // 태그를 여러 개 선택하고 싶을 때
+/* Ex)
+<button>입력<button>
+<button>버튼 2<button>
+<button>버튼 3<button>
+
+document.querySelector('button'); // 버튼 3개가 다 선택됨
+콘솔 창에 NodeList(3) [button.first, button, button] 이렇게 배열 유사품인 객체들이 뜬다.
+function에서 argument를 뭘 넣었는지 볼때 arguments를 사용하면 배열 형식으로 떴는데 그것 역시 마친가지로 객체다.
+array like object 유사배열인 객체들은 리터럴 객체나 다른 객체처럼 이용할 때 제한 사항이 생긴다.
+*/
+
+// 자식태그와 자손태그
+/* <div><span></span><div> span은 div의 자식 태그
+    <div><span><p></p></span></div> p는 div의 자손태그
+    querySelector('div p') p태그를 찾음 공백
+    querySelector('div>span') 자식인 span >
+*/
+
+// 이벤트 리스너
+document.querySelector('button').addEventListener('clcik', function(){});
+addEventListener('event', function) // function 자리에 ()을 넣는 실수를 하면 안됨
+// 입력창에 입력을 한다던지 클릭을 한다던지 등을 이벤트가 발생했다고 하는데 이때 정해놓은 함수를 실행시킬 수 있게 함
+// 이 function을 콜백함수, 리스너함수라 부른다.
+document.querySelector('input').addEventListener('input', (event) => {
+    console.log('글자 입력', event.target.value);
+}) // 사용자가 input에 입력한 글자를 콘솔로그를 통해서 볼 수 있음
